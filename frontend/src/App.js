a8cff61d339dfdc23ab0d30f7005d606a4f7fb7b
Bhavesh Pawar <bhaveshpawar456@gmail.com>
2023-07-02 01:50:13 +0530

    added initial react app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Heading from "./components/Heading";
+
+function App() {
+  return (
+    <div>
+    <Heading />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Heading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <React.Fragment>
+    <center>
+        <h1 className='mt-3 text-primary'> React Django CRUD </h1>
+        <hr />
+    </center>
+    </React.Fragment>
+  )
+}
+
+export default Heading
