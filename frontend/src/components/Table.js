c275bb64aed55dd367daa8373d881f2e60e5554c
Bhavesh Pawar <bhaveshpawar456@gmail.com>
2023-07-06 00:46:57 +0530

    added update form

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,67 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import TableColumns from "./TableColumns";
 import TableRows from "./TableRows";
 
 function MyTable() {
-    const users = {
-        "users": [
-          {
-            "pk_user": 1,
-            "full_name": "Bhavesh Panwar",
-            "father_name": "Nilesh Panwar",
-            "mother_name": "Pooja Panwar",
-            "age": "24",
-            "mobile": "9300144532",
-            "email": "bhaveshpanwar783@gmail.com"
-          },
-          {
-            "pk_user": 2,
-            "full_name": "Dhruv Panwar",
-            "father_name": "Nilesh Panwar",
-            "mother_name": "Pooja Panwar",
-            "age": "16",
-            "mobile": "7879668406",
-            "email": "dhruvpanwar123@gmail.com"
-          },
-          {
-            "pk_user": 3,
-            "full_name": "Kushal Panwar",
-            "father_name": "Mitesh Panwar",
-            "mother_name": "Urmila Panwar",
-            "age": "16",
-            "mobile": "7879668406",
-            "email": "kushal.panwar@gmail.com"
-          },
-          {
-            "pk_user": 4,
-            "full_name": "Yashika Panwar",
-            "father_name": "Mitesh Panwar",
-            "mother_name": "Urmila Panwar",
-            "age": "18",
-            "mobile": "7879668406",
-            "email": "yashika789@gmail.com"
-          },
-          {
-            "pk_user": 8,
-            "full_name": "Mitesh Panwar",
-            "father_name": "Mohanlal Panwar",
-            "mother_name": "Vimla Bai Panwar",
-            "age": "18",
-            "mobile": "7879668406",
-            "email": "mitesh.panwar@gmail.com"
-          },
-          {
-            "pk_user": 10,
-            "full_name": "Nilesh Panwar",
-            "father_name": "Mohanlal Panwar",
-            "mother_name": "Vimla Bai Panwar",
-            "age": "18",
-            "mobile": "9926350022",
-            "email": "nilesh.panwar@gmail.com"
-          }
-        ]
-      };
-      let all_users = users.users;
+    let [users , setUsers ] = useState([]);
+
+    useEffect(()=>{
+      axios
+        .get('http://127.0.0.1:7000/api/getUsers/')
+        .then((res)=>{
+          setUsers(res.data.users);
+        })
+    },[]); 
+
+      let all_users = users;
       let table_rows = all_users.map((user) => (
           <TableRows key={user.pk_user}
               pk={user.pk_user} fname={user.full_name} 
--- frontend/src/components/UpdateUser.js
+++ frontend/src/components/UpdateUser.js
@@ -1,14 +1,62 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, Navigate, useParams } from 'react-router-dom'
+import Heading from './Heading';
+import axios from 'axios';
 
 export default function UpdateUser() {
-    const params = useParams();
-  return (
-    <div>
-    
-    UpdateUser # {params.id}
+  const params = useParams();
+  let [userData, setUser] = useState({pk_user:'',full_name:'',father_name:'',mother_name:'',age:'',mobile:'',email:''});
+  let [isFormSubmitted , setFormSubmit] = useState(false);
 
-    <Link to="/" className='btn btn-primary'> Back </Link>
-    </div>
+  useEffect(() => {
+    axios
+      .get(`http://localhost:7000/api/getUser/${params.id}/`)
+      .then((res) => {
+        console.log(res.data);
+        setUser(res.data.user)
+      });
+  }, [params.id]);
+
+  function submitData(e){
+    e.preventDefault();
+    setFormSubmit(true);
+  }
+
+  return (
+    <>
+      <Heading />
+      <div>
+        <Link to="/" className='btn btn-primary float-end'> Back </Link>
+        <h1 className='text-center text-success mt-2'>Update User</h1>
+        <form onSubmit={(e)=>submitData(e)}>
+          <div className="mt-2">
+            <label htmlFor="name" className="form-label">Full name : </label>
+            <input type="text" onChange={(e)=>setUser({...userData,full_name : e.target.value})} className="form-control" id="name" value={userData.full_name} />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="father_name" className="form-label">Father name : </label>
+            <input type="text" onChange={(e)=>setUser({...userData,father_name : e.target.value})} className="form-control" id="father_name" value={userData.father_name} />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="mother_name" className="form-label">Mother name : </label>
+            <input type="text" onChange={(e)=>setUser({...userData,mother_name : e.target.value})} className="form-control" id="mother_name" value={userData.mother_name} />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="email" className="form-label">Email : </label>
+            <input type="email" onChange={(e)=>setUser({...userData,email : e.target.value})} className="form-control" id="email" value={userData.email} />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="age" className="form-label">Age : </label>
+            <input type="text" onChange={(e)=>setUser({...userData,age : e.target.value})} className="form-control" id="age" value={userData.age} />
+          </div>
+          <div className="mt-2">
+            <label htmlFor="mobile" className="form-label">Mobile : </label>
+            <input type="text" onChange={(e)=>setUser({...userData,mobile : e.target.value})} className="form-control" id="mobile" value={userData.mobile} />
+          </div>
+          <button type="submit" className="btn btn-primary mt-2">Submit</button>
+        </form>
+        {isFormSubmitted && (<Navigate to="/" />)}
+      </div>
+    </>
   )
 }
