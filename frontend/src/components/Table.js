baf5b9ab564801298b67edbeda359392704bd67b
Bhavesh Pawar <bhaveshpawar456@gmail.com>
2023-07-09 17:17:12 +0530

    fixed redirecting bug

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -2,24 +2,48 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import TableColumns from "./TableColumns";
 import TableRows from "./TableRows";
+import { Navigate } from "react-router-dom";
 
 function MyTable() {
     let [users , setUsers ] = useState([]);
+    let [confirmDeleteUser , setConfirmDeleteUser] = useState(false);
+    let [pkUser , setPkUser] = useState(0);
 
-    useEffect(()=>{
+    const confirmDelete = (name,pk) =>{
+      let bool = window.confirm(`Do you want to delete ${name} ${pk}`);
+      if(bool){
+        console.log("Delete it");
+      }
+      setPkUser(pk);
+      setConfirmDeleteUser(bool);
+    }
+    function fetchUsers(){
       axios
-        .get('http://127.0.0.1:7000/api/getUsers/')
+      .get('http://127.0.0.1:7000/api/getUsers/')
+      .then((res)=>{
+        setUsers(res.data.users);
+      })
+    }
+    useEffect(()=>{
+      console.log(confirmDeleteUser);
+      if(confirmDeleteUser){
+        axios
+        .delete(`http://127.0.0.1:7000/api/getUser/${pkUser}/`)
         .then((res)=>{
-          setUsers(res.data.users);
+          console.log(res.data);
+          fetchUsers();
         })
-    },[]); 
+      }
+      fetchUsers()
+        // eslint-disable-next-line
+    },[confirmDeleteUser]); 
 
       let all_users = users;
       let table_rows = all_users.map((user) => (
           <TableRows key={user.pk_user}
               pk={user.pk_user} fname={user.full_name} 
               pname={user.father_name} mname={user.mother_name} age={user.age} 
-              email={user.email} mobile={user.mobile} />
+              email={user.email} mobile={user.mobile} click={confirmDelete} />
       ));
       return (
     <React.Fragment>
@@ -38,6 +62,7 @@ function MyTable() {
       </tbody>
     </table>
     </div>
+    {confirmDeleteUser && (<Navigate to="/" />)}
     </React.Fragment>
   );
 }
--- frontend/src/components/TableRows.js
+++ frontend/src/components/TableRows.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPenToSquare, faTrash} from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
+import '../components/css/delete_user.css';
 
 export default function TableRows(props,key) {
-    const { pk , fname ,pname , mname , age , mobile , email  } = props;
-  return (
+    const { pk , fname ,pname , mname , age , mobile , email ,click } = props;
+
+    return (
     <>
     <tr>
     <td>{pk}</td>
@@ -17,7 +17,7 @@ export default function TableRows(props,key) {
     <td>{mobile}</td>
     <td>{email}</td>
     <td> <Link to={`updateUser/${pk}`} className='text-warning'> <FontAwesomeIcon icon={faPenToSquare} /> </Link></td>    
-    <td className='text-danger'><FontAwesomeIcon icon={faTrash} /></td>
+    <td className='text-danger' onClick={()=>click(fname,pk)} ><FontAwesomeIcon icon={faTrash} className='delete-user-button' /></td>
     </tr>
     </>
   )
