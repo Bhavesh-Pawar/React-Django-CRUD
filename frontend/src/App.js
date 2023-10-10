1ab28530fc4616955b8d695098d2d403f2badf21
Bhavesh Pawar <bhaveshpawar456@gmail.com>
2023-10-11 00:06:00 +0530

    added search filter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Heading from "./components/Heading";
 import MyTable from "./components/Table";
-import {BrowserRouter , Routes , Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpdateUser from "./components/UpdateUser";
 import AddUser from "./components/AddUser";
 import AddButton from "./components/AddButton";
@@ -8,12 +8,12 @@ function App() {
   return (
     <div className="container">
 
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-        <Route path='/' element={<><Heading /> <AddButton /><MyTable /></> } />
-        <Route path='addUser/' element={<><AddUser /></>} />
-        <Route path='updateUser/:id' element={<><UpdateUser /></>} />
-        <Route path='*' element={<><Heading /> <MyTable /></> } />
+          <Route path='/' element={<><Heading /> <AddButton />  <MyTable /></>} />
+          <Route path='addUser/' element={<><AddUser /></>} />
+          <Route path='updateUser/:id' element={<><UpdateUser /></>} />
+          <Route path='*' element={<><Heading /> <MyTable /></>} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import axios from 'axios';
 import TableColumns from "./TableColumns";
 import TableRows from "./TableRows";
@@ -8,6 +8,7 @@ function MyTable() {
   let [users, setUsers] = useState([]);
   let [confirmDeleteUser, setConfirmDeleteUser] = useState(false);
   let [pkUser, setPkUser] = useState(0);
+  let [searchVal, setSearchVal] = useState("");
 
   const confirmDelete = (name, pk) => {
     let bool = window.confirm(`Do you want to delete ${name} ${pk}`);
@@ -16,22 +17,20 @@ function MyTable() {
     setConfirmDeleteUser(bool);
   }
 
-  function fetchUsers() {
-      axios
-        .get('http://127.0.0.1:7000/api/getUsers/')
-        .then((res) => {
-          setUsers(res.data.users);
-        })
-        .catch(error => {
-          console.log("Server is down");
-        }); 
-
-  }
-
+  const fetchUsers = useCallback(() => {
+    axios
+      .get('http://127.0.0.1:7000/api/getUsers/')
+      .then((res) => {
+        setUsers(res.data.users);
+      })
+      .catch(error => {
+        console.log("Server is down");
+      });
+  }, []);
 
   useEffect(() => {
     if (confirmDeleteUser) {
-        axios
+      axios
         .delete(`http://127.0.0.1:7000/api/getUser/${pkUser}/`)
         .then((res) => {
           console.log(res.data);
@@ -39,26 +38,34 @@ function MyTable() {
         })
         .catch(error => {
           console.log("Server is down");
-        });      
+        });
     }
 
-    fetchUsers()
+    fetchUsers();
+  }, [confirmDeleteUser, pkUser, fetchUsers]);
 
-    // eslint-disable-next-line
-  }, [confirmDeleteUser]);
-
-  let all_users = users;
-  let table_rows = all_users.map((user) => (
+  // Filter users based on search value
+  const filteredUsers = searchVal ? users.filter(user => user.full_name.includes(searchVal)) : users;
+  let table_rows = filteredUsers.map(user => (
     <TableRows key={user.pk_user}
       pk={user.pk_user} fname={user.full_name}
       pname={user.father_name} mname={user.mother_name} age={user.age}
       email={user.email} mobile={user.mobile} click={confirmDelete} />
   ));
+
   return (
     <React.Fragment>
-      <h3 className="text-center">
-        Users Table
-      </h3>
+      <h3 className="text-center">Users Table</h3>
+
+      <div className="row g-3 align-items-center">
+        <div className="col-auto">
+          <label htmlFor="full_name" className="col-form-label">Full Name</label>
+        </div>
+        <div className="col-auto">
+          <input type="text" onChange={(e) => setSearchVal(e.target.value)} id="full_name" className="form-control" />
+        </div>
+      </div>
+
       <div className="table-responsive">
         <table className="table table-bordered mt-2">
           <thead>
